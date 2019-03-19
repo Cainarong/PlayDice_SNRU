f90f9316bbe7588b95944bbfcc549995fc88146b
masterToey <masterToey@abc.com>
2019-03-19 11:42:55 +0700

    Get Event From Random Button

--- PlayDice/ViewController.swift
+++ PlayDice/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstDicelmageView: UIImageView!
+    
+    
+    @IBOutlet weak var secondDiceimageView: UIImageView!
+    
+    
+    @IBOutlet weak var thirdDiceimageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }//Main Method
+    
+    @IBAction func redomDiceButtom(_ sender: UIButton) {
+        
+        print("You Click Remdom")
+        
+        
+        
+    }//remdomDice
+    
+    
+    
 
 
-}
+}  //Main Class
 
